cd4a3a536d142ee22c907e1439ed490ff3003c46
Prim_mm94 <Prim_mm94@student.itstep.org>
2023-05-08 19:26:03 +0300

    Add ViewModel

---
+++ TorrentTrackerApp/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorrentTrackerApp
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ViewModel
+    {
+        ObservableCollection<CurrentTorrentFile> torrents;
+        public ViewModel() 
+        {
+            torrents= new ObservableCollection<CurrentTorrentFile>();
+        }
+        public void Add(CurrentTorrentFile file)
+        {
+            torrents.Add(file);
+        }
+    }
+    [AddINotifyPropertyChangedInterface]
+    public class CurrentTorrentFile
+    {
+        public string Name { get; set; }
+        public double Size { get; set; }
+        public double Speed { get; set; }
+        public double DownloadProgress { get; set; }
+    }
+}
