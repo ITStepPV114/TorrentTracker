7c33b57971e10a215b5d6fb3c70171e511d55ccb
Slonets <bogdan.slonets@gmail.com>
2023-05-12 12:58:12 +0300

    Fix mistakes

---
+++ DateBase/Interface/ILoginService.cs
@@ -0,0 +1,15 @@
+﻿using DateBase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateBase.Interface
+{
+    public interface ILoginService
+    {
+        public User Login(string username, string password);
+        public bool SingUp(string username, string password);
+    }
+}
